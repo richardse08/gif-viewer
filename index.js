5b25be2c5baa57167204c9a6c4b36d079543856c
richardse08 <eric.richards.finance@gmail.com>
2018-07-02 19:46:15 -0400

    removed extra pagination from top

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const https = require('https');
 const path = require('path');
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
